ce52e74381caf625344f8bcd93fa444baa71c484
James Gore <73591234+jgore077@users.noreply.github.com>
2024-09-12 13:59:29 -0400

    init repo

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='Visual-Contextual Classifier',
+      version='1.0',
+      description='Predicts whether a sentence is a visual sentence or a contextual sentence',
+      author='James Gore',
+      author_email='james.gore@maine.edu',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=setuptools.find_packages(),
+     )
---
+++ src/VisualContextualClassifier.py
@@ -0,0 +1,5 @@
+MODEL_URL="https://cs.usm.maine.edu/~behrooz.mansouri/visual_contextual_classifier/"
+
+class VisualContextualClassifier():
+    def __init__(self) -> None:
+        pass
